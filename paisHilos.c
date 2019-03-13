22d506c8324dbf4eef03058a27e43daef2cf23cc
Pietro <pietroiaia98@gmail.com>
2019-03-13 19:04:36 -0400

    Agregado hilo Prensa con Hemeroteca y semaforo

--- paisHilos.c
+++ paisHilos.c
@@ -7,11 +7,16 @@
 #define TRUE 1
 #define FALSE 0
 
+char* PrensaLine=NULL;
+int day=1;
+pthread_mutex_t mutex;
+
 void *threadEjec(void *vargp) 
 { 
     size_t len = 0;
     char* line;
-    int Encontro = FALSE;                                                                    
+    int Encontro = FALSE; 
+    char lines[1000];                                                                   
     FILE* fp = fopen("Ejecutivo.acc", "r");
 
     //Encuentra una accion, con 20% de probabilidad
@@ -30,6 +35,8 @@ void *threadEjec(void *vargp)
         }
     }
     printf("%s", line);
+    pthread_mutex_lock(&mutex);                                                     //Se hace lock del mutex y se libera cuando Prensa haya terminado de imprimir y agregado a Hemeroteca
+    PrensaLine = line;
     fclose(fp);
     
     pthread_exit(NULL);
@@ -59,6 +66,8 @@ void *threadLegis(void *vargp)
         }
     }
     printf("%s", line);
+    pthread_mutex_lock(&mutex);                                                      //Se hace lock del mutex y se libera cuando Prensa haya terminado de imprimir y agregado a Hemeroteca
+    PrensaLine = line;
     fclose(fp);
 
     pthread_exit(NULL);
@@ -88,19 +97,50 @@ void *threadJud(void *vargp)
         }
     }
     printf("%s", line);
+    pthread_mutex_lock(&mutex);                                                      //Se hace lock del mutex y se libera cuando Prensa haya terminado de imprimir y agregado a Hemeroteca
+    PrensaLine = line;
     fclose(fp);
 
     pthread_exit(NULL);
+}
+
+/*Este hilo es la prensa y se encarga de hacer print de las acciones fracazadas o logradas*/
+void *threadPrensa(void *vargp)
+{
+
+    char Hemeroteca[100][1000];
+    char copy[1000];
+    for(int i=0; i<100; i++){
+        memset(Hemeroteca[i], 0, sizeof(Hemeroteca[i]));
+    }
+    while(TRUE){
+        while(PrensaLine==NULL){                                      //Cuando se le pasa un valos a PrensaLine, la prensa lo imprime
+
+        }
+        printf("%d.%s", day, PrensaLine);
+        strcpy(copy, PrensaLine);                                     //Hace un duplicado de el pointer
+        strcpy(Hemeroteca[day-1], copy);                              //Coloca el duplicado en la Hemeroteca
+        for(int i=0; i<day; i++){                                     //Revisa cada string en hemeroteca
+            printf("Hemero: %s",Hemeroteca[i]);
+        }
+        memset(copy, 0, sizeof(copy));                                //Se vacia copy
+        PrensaLine = NULL;                                            //Se vuelve NULL el apuntador
+        day++;
+        pthread_mutex_unlock(&mutex);                                 //Aqui se libera el Mutex
+    }
+
 }
 
 void main(int argc, char *argv[]){
     pthread_t thread_id_ejec;
     pthread_t thread_id_legis;
     pthread_t thread_id_jud;
+    pthread_t thread_id_prensa;
 
     pthread_create(&thread_id_ejec, NULL, threadEjec, NULL);
     pthread_create(&thread_id_legis, NULL, threadLegis, NULL);
     pthread_create(&thread_id_jud, NULL, threadJud, NULL);
+    pthread_create(&thread_id_jud, NULL, threadPrensa, NULL);
 
     getchar();
 
